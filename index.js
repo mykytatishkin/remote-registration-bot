2d7be04bc83da560074e326dbcb1c8f62be6e50d
Mykyta Tishkin <nikita.tishkin@outlook.com>
2024-06-16 00:50:15 +0200

    migration from v13 to v14

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const { Client, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-});
-
+// Log in to Discord with your client's token
 client.login(token);
