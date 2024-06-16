4ed5792cc0f0a213e87b1ab5fb0d008d7c452e70
Mykyta Tishkin <nikita.tishkin@outlook.com>
2024-06-16 12:43:52 +0200

    commands logging

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ for (const folder of commandFolders) {
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
+        console.log(`Loading command file: ${filePath}`); // Добавьте это логирование
         const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
+            console.log(`Command ${command.data.name} loaded`); // Добавьте это логирование
         } else {
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
